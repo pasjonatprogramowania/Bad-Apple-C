1a9a6eed3c0ba635d5eb9c69ecbdbb5d0866f4fd
pasjonatprogramowania <44599809+pasjonatprogramowania@users.noreply.github.com>
2022-11-16 09:24:43 +0100

    remove not esentail code

--- main.c
+++ main.c
@@ -8,31 +8,3 @@ int main()
     printf("e");
     return 0;
 }
-
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int main() {
-//    FILE    *textfile;
-//    char    *text;
-//    long    numbytes;
-//
-//    textfile = fopen("./plik.txt", "rb");
-//    if(textfile == NULL)
-//        return 1;
-//
-//    fseek(textfile, 0L, SEEK_END);
-//    numbytes = ftell(textfile);
-//    fseek(textfile, 0L, SEEK_SET);
-//
-//    text = (char*)calloc(numbytes, sizeof(char));
-//    if(text == NULL)
-//        return 1;
-//
-//    fread(text, sizeof(char), numbytes, textfile);
-//    fclose(textfile);
-//
-//    printf(text);
-//
-//    return 0;
-//}
