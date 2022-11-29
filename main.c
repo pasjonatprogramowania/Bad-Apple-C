40582c647b1e6826ac4fc3ab5228dcb8101ded5e
pasjonatprogramowania <44599809+pasjonatprogramowania@users.noreply.github.com>
2022-11-29 10:24:38 +0100

    reading rgb values works but file is compressed

--- main.c
+++ main.c
@@ -1,30 +1,68 @@
 #include <stdio.h>
+#include <malloc.h>
 
-struct BITMAP_header {
+struct BIT_MAP_HEADER {
     char name[2];
     unsigned int size;
     int garbage;
-    unsigned int image_offset;
+    unsigned int imageOffset;
 };
-struct DIB_header {
+struct BIT_MAP_INFO_HEADER {
     unsigned int header_size;
     unsigned int width;
     unsigned int height;
-    unsigned int color_plane;
-    unsigned int bit_per_pixel;
+    unsigned int colorPlane;
+    unsigned int bitPerPixel;
+    unsigned int compression;
+    unsigned int imageSize;
 };
+struct RGB {
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+};
+struct Image{
+    unsigned int width;
+    unsigned int height;
+    struct RGB **rgb;
+};
+struct Image readImage(FILE *file,int height,int width){
+    struct Image image;
+    image.rgb = (struct RGB **) malloc(height*sizeof(void*));
+    image.width = width;
+    image.height = height;
+    int i;
+    for( i=height-1;i==0;i--){
+        image.rgb[i] = (struct RGB *) malloc(width*sizeof(struct RGB));
+        fread(image.rgb[i],width,sizeof(struct RGB),file);
+    }
+    return image;
+};
+void freeImage(struct Image image){
+    int i;
+    for(i=image.height-1;i==0;i--) free(image.rgb[i]);
+}
+//unsigned char grayscaleImage(struct Image image){
+//    int i;
+//    for(i=image.height-1;i)
+//}
 int main()
 {
     FILE *file = fopen("C:\\Users\\admin\\CLionProjects\\bit\\bialy.bmp","rb");
-    if (file == NULL)
-        return NULL;
-    struct BITMAP_header header;
-    struct DIB_header dibheader;
+    if (file == NULL) return 1;
+    struct BIT_MAP_HEADER header;
+    struct BIT_MAP_INFO_HEADER dibheader;
     fread(header.name,2,1,file);
+    if ((header.name[0]!='B')||(header.name[1]!='M')){ fclose(file); return 1; }
     fread(&header.size,3*sizeof(int),1,file);
-//    fread(&header, sizeof(struct BITMAP_header), 1, file);
-    fread(&dibheader,sizeof(struct DIB_header),  1, file);
-    printf("Size %d\nWidth %d\nHeight %d\n",dibheader.header_size,dibheader.width,dibheader.height);
+    fread(&dibheader, sizeof(struct BIT_MAP_INFO_HEADER), 1, file);
+
+//    if((dibheader.header_size !=40) || (dibheader.compression != 0) || (dibheader.bitPerPixel!=24)){
+//        fclose(file); return 1;
+//    }
+    fseek(file, header.imageOffset, SEEK_SET);
+    struct Image image = readImage(file, dibheader.height, dibheader.width);
     fclose(file);
+    freeImage(image);
     return 0;
 }
